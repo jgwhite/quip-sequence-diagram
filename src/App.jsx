2d9c737cc523f5403e91c3c37732e84a05c817f3
Jamie White <jamie@jgwhite.co.uk>
2019-04-03 07:11:46 +0100

    Plumb in focus, menu, edit-mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,88 @@
-import Styles from "./App.less";
-
 export default class App extends React.Component {
-    render() {
-        return <div className={Styles.hello}>Hello, world!</div>;
+  state = {
+    isFocused: false,
+    isEditing: false
+  };
+
+  render() {
+    let src = this.props.rootRecord.get("src");
+    let width = quip.apps.getContainerWidth();
+    let { isFocused, isEditing } = this.state;
+
+    return (
+      <div
+        style={{
+          width: `${width}px`,
+          background: isEditing ? "pink" : "",
+          outline: isFocused ? "1px solid red" : ""
+        }}
+      >
+        {src}
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    quip.apps.addEventListener(quip.apps.EventType.FOCUS, this.onFocus);
+    quip.apps.addEventListener(quip.apps.EventType.BLUR, this.onBlur);
+    this.props.rootRecord.listen(this.onChange);
+    this.updateMenu();
+  }
+
+  componentDidUpdate() {
+    this.updateMenu();
+  }
+
+  componentWillUnmount() {
+    quip.apps.removeEventListener(quip.apps.EventType.FOCUS, this.onFocus);
+    quip.apps.removeEventListener(quip.apps.EventType.BLUR, this.onBlur);
+    this.props.rootRecord.unlisten(this.onChange);
+  }
+
+  updateMenu() {
+    if (this.state.isEditing) {
+      quip.apps.updateToolbar({
+        toolbarCommandIds: ["edit"],
+        menuCommands: [
+          {
+            id: "edit",
+            label: "Done",
+            handler: () => this.setState({ isEditing: false })
+          }
+        ]
+      });
+    } else {
+      quip.apps.updateToolbar({
+        toolbarCommandIds: ["edit"],
+        menuCommands: [
+          {
+            id: "edit",
+            label: "Edit",
+            handler: () => this.setState({ isEditing: true })
+          }
+        ]
+      });
     }
+  }
+
+  editorDidMount(editor) {
+    editor.focus();
+    this.editor = editor;
+  }
+  onFocus = () => {
+    this.setState({ isFocused: true });
+    this.updateMenu();
+  };
+
+  onBlur = () => {
+    this.setState({ isFocused: false, isEditing: false });
+    this.updateMenu();
+  };
+
+  onChange = () => {
+    if (this.state.isEditing) {
+      return;
+    }
+    this.forceUpdate();
+  };
 }
--- src/root.jsx
+++ src/root.jsx
@@ -1,8 +1,26 @@
 import quip from "quip";
 import App from "./App.jsx";
 
+export class RootRecord extends quip.apps.RootRecord {
+  static getProperties() {
+    return {
+      src: "string"
+    };
+  }
+
+  static getDefaultProperties() {
+    return {
+      src: "title Example\nAlice -> Bob: Hello\nBob -> Alice: Hi"
+    };
+  }
+}
+
+quip.apps.registerClass(RootRecord, "root-record");
+
 quip.apps.initialize({
-    initializationCallback: function(rootNode) {
-        ReactDOM.render(<App/>, rootNode);
-    },
+  initializationCallback(rootNode) {
+    let rootRecord = quip.apps.getRootRecord();
+
+    ReactDOM.render(<App rootRecord={rootRecord} />, rootNode);
+  }
 });
